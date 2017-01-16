e577dde9cdd52d2c31972212206c59181360861b
maiconbaumx <maiconbaumx@gmail.com>
2017-01-16 16:48:37 -0200

    Update automate.sh

--- aws-scripts-mon/automate.sh
+++ aws-scripts-mon/automate.sh
@@ -1,14 +1,10 @@
-/root/aws-scripts-mon/mon-put-instance-data.pl --mem-util
-/root/aws-scripts-mon/mon-put-instance-data.pl --mem-used
-/root/aws-scripts-mon/mon-put-instance-data.pl --mem-avail 
-/root/aws-scripts-mon/mon-put-instance-data.pl --swap-util
-/root/aws-scripts-mon/mon-put-instance-data.pl --swap-used
+/root/aws-scripts-mon/mon-put-instance-data.pl --mem-util  --mem-used --mem-avail --swap-util --swap-used 
 
 START=2
 COUNTER=`df -h | awk '{print $6}' | wc -l`
          while [  $START -lt $COUNTER ]; do
-             /root/aws-scripts-mon/mon-put-instance-data.pl --disk-path=$(df -h | awk '{print $6}' | grep -n ^ | grep ^$START: | cut -d: -f2)  --disk-space-util
-             /root/aws-scripts-mon/mon-put-instance-data.pl --disk-path=$(df -h | awk '{print $6}' | grep -n ^ | grep ^$START: | cut -d: -f2)  --disk-space-used
-             /root/aws-scripts-mon/mon-put-instance-data.pl --disk-path=$(df -h | awk '{print $6}' | grep -n ^ | grep ^$START: | cut -d: -f2)  --disk-space-avail 
+             /root/aws-scripts-mon/mon-put-instance-data.pl --disk-path=$(df -h | awk '{print $6}' | grep -n ^ | grep ^$START: | cut -d: -f2)  --disk-space-used --disk-space-avail --disk-space-util --aws-credential-file=/root/aws-scripts-mon/awscreds.template
              let START=START+1
          done
+         
+/root/aws-scripts-mon/mon-put-instance-data-raiz.pl --disk-space-util --disk-space-used --disk-space-avail --disk-path=/
