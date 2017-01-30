7b56614604627b6422566f453a5f992f235ef01f
maiconbaumx <maiconbaumx@gmail.com>
2017-01-30 14:12:51 -0200

    Update automate.sh

--- aws-scripts-mon/automate.sh
+++ aws-scripts-mon/automate.sh
@@ -3,7 +3,7 @@
 COUNTER=$(df -h | awk '{print $6}' | awk '{if (NR!=1) {print}}' | wc -l)
          for i in `seq 1 $COUNTER`;
          do
-             ~/autommdm/aws-scripts-mon/mon-put-instance-data.pl --disk-path=`df -h | awk '{print $6}' | grep -n ^ | cut -d: -f2 | awk '{if (NR!=1) {print}}' | awk NR==$i`  --disk-space-used --disk-space-avail --disk-space-util --verbose #--aws-credential-file=~/autommdm/aws-scripts-mon/awscreds.template
+             ~/autommdm/aws-scripts-mon/mon-put-instance-data.pl --disk-path=`df -h | awk '{print $6}' | grep -n ^ | cut -d: -f2 | awk '{if (NR!=1) {print}}' | awk NR==$i`  --disk-space-used --disk-space-avail --disk-space-util #--aws-credential-file=~/autommdm/aws-scripts-mon/awscreds.template
          done
 ~/autommdm/aws-scripts-mon/mon-put-instance-data-raiz.pl --disk-space-util --disk-space-used --disk-space-avail --disk-path=/ #--aws-credential-file=~/autommdm/aws-scripts-mon/awscreds.template
 
